e5f924d52c1e8e30382645c9e47ab0a96181b589
gunianghaoxiong <yinghualuogf@protonmail.com>
2024-10-15 20:25:22 +0800

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,8 +2,8 @@
   <footer class="bg-white px-4">
     <div class="mx-auto max-w-screen-xl py-4">
       <div class="text-gray-500 text-sm text-center">
-        Powered by
-        <a target="_blank" href="/" class="underline">@iDataRiver.</a>
+        联系我们：
+        <a target="_blank" href="https://tawk.to/chat/6700789137379df10df1fc1a/1i9ctdu3l" class="underline">在线客服</a>
       </div>
     </div>
   </footer>
